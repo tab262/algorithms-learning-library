626c56db228fc7a25dec99531efb3e49e6ea3d61
Jacob Janco <jancojt@gmail.com>
2013-05-28 16:28:59 -0400

    tabbing

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -6,13 +6,12 @@ using namespace std;
 void insertion_sort(double array_in[], int len)
 {
     for (int i = 1; i < len; i++)
-	{
+    {
 		double current_element = array_in[i];
 		int j;
 		for (j = i - 1; j >= 0 && array_in[j] > current_element; j--)
 		{
 			array_in[j + 1] = array_in[j];
-
 		}
 
 		array_in[j + 1] = current_element;
@@ -30,7 +29,3 @@ void array_printer(double array_in[], int len)
 	cout << endl;
 	for (int i = 0; i < 50; i++) { cout << "-"; } cout << endl;
 }
-
-
-
-
